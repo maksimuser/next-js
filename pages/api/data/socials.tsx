2d01e926dcc85d2a9d10ed9e8b56f3726013ca9b
Maksym Dema <maksym.dema@cd.p2h.com>
2022-10-19 21:12:15 +0200

    fixed socials

--- pages/api/data/socials.tsx
+++ pages/api/data/socials.tsx
@@ -1,27 +1,18 @@
 export const socials = [
   {
     id: 1,
-    icon: "youtube",
-    path: "https://youtube.com/YauhenKavalchuk",
+    icon: 'youtube',
+    path: 'https://youtube.com',
   },
   {
     id: 2,
-    icon: "instagram",
-    path: "https://instagram.com/YauhenKavalchuk",
+    icon: 'instagram',
+    path: 'https://instagram.com',
   },
+
   {
     id: 3,
-    icon: "linkedin",
-    path: "https://linkedin.com/in/YauhenKavalchuk",
-  },
-  {
-    id: 4,
-    icon: "vk",
-    path: "https://vk.com/YauhenKavalchuk",
-  },
-  {
-    id: 5,
-    icon: "twitter",
-    path: "https://twitter.com/YauhenKavalchuk",
+    icon: 'twitter',
+    path: 'https://twitter.com',
   },
 ];
